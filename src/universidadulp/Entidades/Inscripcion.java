23101f0c568e8a275fad5b6f4148ed6571edb911
BrunoPalmucci <brunoapalmucci@sanluis.edu.ar>
2023-10-10 11:00:29 -0300

    Agregadas las clases de entidades
    Agregadas Inscripcion y Materia.

---
+++ src/universidadulp/Entidades/Inscripcion.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadulp.Entidades;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Inscripcion {
+    private int idInscripcion;
+    Alumno alumno;
+    Materia materia;
+    double nota;
+
+    public Inscripcion(int idInscripcion, Alumno alumno, Materia materia, double nota) {
+        this.idInscripcion = idInscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public int getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    
+    
+    
+}
