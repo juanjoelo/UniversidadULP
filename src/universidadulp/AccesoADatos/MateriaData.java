5a7e1a5f6532d9a96eeda105590ed84691bc471f
juanjoelo <juanjoel.olguin@gmail.com>
2023-10-12 11:22:36 -0300

    Construccion materiaData

---
+++ src/universidadulp/AccesoADatos/MateriaData.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadulp.AccesoADatos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import universidadulp.Entidades.Materia;
+
+/**
+ *
+ * @author juan_
+ */
+public class MateriaData {
+    Connection con;
+    
+    
+    public void guardarMateria(Materia mat){
+        String sql = "INSERT INTO materia (nombre, año, estado) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, mat.getNombre());
+            ps.setInt(2, mat.getAnioMateria());
+            ps.setBoolean(3, mat.isActivo());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                mat.setIdMateria(rs.getInt("idMateria"));
+                JOptionPane.showMessageDialog(null, "Materia añadida con exito.");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Materia: "+ex.getMessage());
+        }
+    }
+    
+    public Materia buscarMateria(int id){
+        Materia materia = null;
+        
+        String sql="SELECT nombre, año FROM materia WHERE idMateria = ? AND estado = 1";
+        PreparedStatement ps = null;
+        
+        try{
+        ps=con.prepareStatement(sql);
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+        
+            if (rs.next()) {
+                materia=new Materia();
+                materia.setIdMateria(id);
+                materia.setNombre("nombre");
+                materia.setAnioMateria(rs.getInt("año"));
+                materia.setActivo(true);
+                JOptionPane.showMessageDialog(null, materia);
+     
+            }
+            else{
+                JOptionPane.showConfirmDialog(null, "No existe la materia");
+            }
+            
+            ps.close();  
+        } catch (SQLException ex) {
+   JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Alumno "+ex.getMessage()); 
+   }
+        return materia;
+    }
+    
+    public void modificarMateria(Materia mat){
+        String sql = "UPDATE materia SET nombre = ?, año = ? WHERE idMateria = ? ";
+        PreparedStatement ps = null;
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, mat.getNombre());
+            ps.setInt(2, mat.getAnioMateria());
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Modificado Exitosamente.");
+            }else {
+            JOptionPane.showMessageDialog(null, "La materia no existe");
+        }
+        } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Materia "+ex.getMessage());
+    }
+        
+        
+        
+    }
+    
+    public void eliminnarMateria(int id){
+            try {
+                String sql= "UPDATE materia SET estado = 0 WHERE idMateria = ?";
+                PreparedStatement ps = con.prepareStatement(sql);
+                ps.setInt(1, id);
+                int fila=ps.executeUpdate();
+                if(fila==1){
+                JOptionPane.showMessageDialog(null, " Se eliminó la materia.");
+                }
+                ps.close();
+            } catch (Exception e) {
+            }
+        }
+    
+    
+    public List<Materia> listarMaterias(){
+        List<Materia> materias = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM materia WHERE estado = 1 ";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {                
+                Materia materia = new Materia();
+                materia.setIdMateria(rs.getInt("idMateria"));
+                materia.setNombre("nombre");
+                materia.setAnioMateria(rs.getInt("año"));
+                materias.add(materia);
+                
+            }
+            JOptionPane.showMessageDialog(null, materias);
+            ps.close();
+            
+        } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, " Error al acceder a la tabla Materia "+ex.getMessage());
+        }
+        return materias;
+    }
+    
+    
+    
+    
+    
+}
---
+++ universidadulp.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-10-2023 a las 15:36:12
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `universidadulp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE `alumno` (
+  `idAlumno` int(11) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `fechaNacimiento` date NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `alumno`
+--
+
+INSERT INTO `alumno` (`idAlumno`, `dni`, `apellido`, `nombre`, `fechaNacimiento`, `estado`) VALUES
+(1, 456789, 'Olguin', 'Joel', '1994-12-26', 1),
+(6, 456455, 'Bruno', 'Palmucci', '2000-05-01', 1),
+(8, 7854, 'meesi', 'Lionel', '1986-06-24', 1),
+(15, 123123, 'menem', 'carlos', '1930-07-02', 0),
+(18, 657567, 'Araujo', 'Raul', '1930-03-14', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inscripcion`
+--
+
+CREATE TABLE `inscripcion` (
+  `idInscripto` int(11) NOT NULL,
+  `nota` int(11) DEFAULT NULL,
+  `idAlumno` int(11) NOT NULL,
+  `idMateria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `inscripcion`
+--
+
+INSERT INTO `inscripcion` (`idInscripto`, `nota`, `idAlumno`, `idMateria`) VALUES
+(1, 9, 1, 1),
+(2, 7, 1, 2),
+(5, 9, 6, 1),
+(6, 8, 6, 2),
+(7, 7, 8, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materia`
+--
+
+CREATE TABLE `materia` (
+  `idMateria` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `año` int(11) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `materia`
+--
+
+INSERT INTO `materia` (`idMateria`, `nombre`, `año`, `estado`) VALUES
+(1, 'Lengua', 6, 1),
+(2, 'matematicas', 6, 1),
+(3, 'Sociología', 6, 1),
+(4, 'Calculo', 6, 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`idAlumno`),
+  ADD UNIQUE KEY `dni` (`dni`);
+
+--
+-- Indices de la tabla `inscripcion`
+--
+ALTER TABLE `inscripcion`
+  ADD PRIMARY KEY (`idInscripto`),
+  ADD UNIQUE KEY `idAlumno` (`idAlumno`,`idMateria`),
+  ADD KEY `inscripcion_ibfk_2` (`idMateria`);
+
+--
+-- Indices de la tabla `materia`
+--
+ALTER TABLE `materia`
+  ADD PRIMARY KEY (`idMateria`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  MODIFY `idAlumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `inscripcion`
+--
+ALTER TABLE `inscripcion`
+  MODIFY `idInscripto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `materia`
+--
+ALTER TABLE `materia`
+  MODIFY `idMateria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `inscripcion`
+--
+ALTER TABLE `inscripcion`
+  ADD CONSTRAINT `inscripcion_ibfk_1` FOREIGN KEY (`idAlumno`) REFERENCES `alumno` (`idAlumno`),
+  ADD CONSTRAINT `inscripcion_ibfk_2` FOREIGN KEY (`idMateria`) REFERENCES `materia` (`idMateria`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
